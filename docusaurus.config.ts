45bd7c20a52032ce8a275bc0ef74630f51157e8b
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-03 17:44:35 -0300

    Modificado nome da organização

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "academiadoprogramador-fullstack", // Usually your GitHub org/user name.
+  organizationName: "academiadoprogramador-backend", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
   markdown: {
